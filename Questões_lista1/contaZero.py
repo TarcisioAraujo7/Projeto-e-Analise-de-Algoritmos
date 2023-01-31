c8c97f3036b990e2f060afa04b7bbfc027f15e55
Tarcisio Almeida <tarcisio.olv@gmail.com>
2023-01-31 09:39:49 -0300

    Update contaZero.py

--- Questões_lista1/contaZero.py
+++ Questões_lista1/contaZero.py
@@ -43,3 +43,40 @@ def contaZeros(X,n):
 lista = [0,0,0,0,0,0,0,0,0,1]
 
 print(contaZeros(lista,len(lista)))
+
+# Returns index of x in arr if present, else -1
+def binary_search(arr, low, high, x):
+ 
+    # Check base case
+    if high >= low:
+ 
+        mid = (high + low) // 2
+ 
+        # If element is present at the middle itself
+        if arr[mid] == 1 and arr[mid-1] == 0 :
+            return mid
+        elif arr[mid] == 0 and arr[mid+1] == 1:
+            return mid+1
+        elif arr[high] == 0:
+            return high + 1
+        elif arr[low] == 0 and arr[low+1]== 1:
+            return low + 1
+        # If element is smaller than mid, then it can only
+        # be present in left subarray
+        elif arr[mid] > x:
+            return binary_search(arr, low, mid - 1, x)
+ 
+        # Else the element can only be present in right subarray
+        else:
+            return binary_search(arr, mid + 1, high, x)
+ 
+    else:
+        # Element is not present in the array
+        return -1
+ 
+# Test array
+arr = [ 2, 3, 4, 10, 40 ]
+x = 10
+ 
+# Function call
+result = binary_search(arr, 0, len(arr)-1, x)
