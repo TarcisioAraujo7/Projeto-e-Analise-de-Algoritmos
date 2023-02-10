44f0a70deb133091825615915ef951036800f4e7
Tarcisio Almeida <tarcisio.olv@gmail.com>
2023-02-09 22:00:40 -0300

    DOIDERA

--- Questões_lista2/conta_inversao.py
+++ Questões_lista2/conta_inversao.py
@@ -6,99 +6,55 @@ no pior caso. Por exemplo, o vetor [1, 3, 4, 2] tem duas inversões pois possui 
 (i,j) que obedecem à restrição, que são, (2,4) e (3,4).
 '''
 
-def intercala(Y, comeco, meio, fim):
-
-
-
-    tamanhoA = meio - comeco + 1
-    tamanhoB = fim - meio 
-    
-    print(tamanhoA, " - ", tamanhoB)
-    print("Começo", comeco)
-    print("Meio", meio)
-    print("Fim", fim)
-
-    '''if tamanhoA > tamanhoB:
-        tamanhoA = tamanhoA - 1
-        tamanhoB = tamanhoB + 1
-
-        vetorA = [0] * (tamanhoA)
-        vetorB = [0] * (tamanhoB)
-
-        for x in range(0, tamanhoA):
-            vetorA[x] = Y[comeco + x]
-    
-        for z in range(0, tamanhoB):
-            vetorB[z] = Y[meio  + z ]
-    else:'''
-
-    vetorA = [0] * (tamanhoA)
-    vetorB = [0] * (tamanhoB)
-    for x in range(0, tamanhoA):
-        vetorA[x] = Y[comeco + x]
-    
-    for z in range(0, tamanhoB):
-        vetorB[z] = Y[meio + 1 + z ]
-    
-    #[9,3,0,5,8,4]
-    
-    print(vetorA, vetorB)
-    print('\n')
-
-    i = 0
-    j = 0 
+def merge(A, aux, comeco, meio, fim):
+ 
     k = comeco
+    i = comeco
+    j = meio + 1
+    contador = 0
+    
+    while i <= meio and j <= fim:
 
-    while i < tamanhoA-1 and j < tamanhoB:
-        print("Comparação:",vetorA[i] , vetorB[j]  )
-        if vetorA[i] <= vetorB[j]:
-            Y[k] = vetorA[i]
-            i = i + 1 
+        if A[i] <= A[j]:
+            aux[k] = A[i]
+            i = i + 1
         else:
-            Y[k] = vetorB[j]
-            j = j + 1 
-
-        if vetorA[i+1] <= vetorB[j]:
-            print(vetorA[i+1], vetorB[j])
-             
-
+            aux[k] = A[j]
+            j = j + 1
+            contador += (meio - i + 1)        
+ 
         k = k + 1
-
-
-    while i < tamanhoA:
-        Y[k] = vetorA[i]
+ 
+    while i <= meio:
+        aux[k] = A[i]
+        k = k + 1
         i = i + 1
-        k = k + 1
-
-
-    while j < tamanhoB:
-        Y[k] = vetorB[j]
-        j = j + 1
-        k = k + 1
+ 
+    for i in range(comeco, fim + 1):
+        A[i] = aux[i]
     
-    
-def mergeSort(X, esq,dir):
-    if esq < dir:
-
-        meio = (dir + esq) // 2
-        
-        mergeSort(X, esq, meio)
-        mergeSort(X, meio+1, dir)
-        intercala(X,esq,meio,dir)
+    return contador
+ 
+ 
+def mergesort(A, aux, comeco, fim):
+     
+    if fim <= comeco:        
+        return 0
+ 
+    meio = comeco + (fim - comeco) // 2
+    contador = 0
+ 
+    contador += mergesort(A, aux, comeco, meio)     
+    contador += mergesort(A, aux, meio + 1, fim)  
+    contador += merge(A, aux, comeco, meio, fim)   
+ 
+    return contador
 
 lista = [9,3,0,5,8,4]
-'''
-   (9,3)-
-   (9,0)
-   (9,5)-
-   (9,8)
-   (9,4)-
-   (3,0)-
-   (5,4)-
-   (8,4)
-    '''
+
+
 f = len(lista)
 
-mergeSort(lista, 0, f-1)
+aux = [0] * f
 
-print(lista)
+print(mergesort(lista, aux, 0, f-1))
