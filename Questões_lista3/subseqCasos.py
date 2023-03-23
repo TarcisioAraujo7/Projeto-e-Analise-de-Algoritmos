6dadc6b08e22315690d3852af96ac6c910ccfd6a
Tarcisio Almeida <91756723+TarcisioAraujo7@users.noreply.github.com>
2023-03-23 14:46:13 -0300

    Update subseqCasos.py

--- Questões_lista3/subseqCasos.py
+++ Questões_lista3/subseqCasos.py
@@ -10,6 +10,7 @@ def maior_subsequencia_crescente(casos):
     return pred
 
 casos = [1800, 1900, 1850, 1955, 1750, 2100, 2005, 2215, 2155]
+casos1 = [1,5,2,3]
 # print(maior_subsequencia_crescente(casos)) # saída: 5
 
 # [1,5,2,3]
@@ -51,6 +52,17 @@ def maior_valor(lista):
     
     return maior
 
+def indice_maior_valor(lista):
+    if not lista:
+        return None
+    
+    maior = lista[0]
+    for i in range(1, len(lista)):
+        if lista[i] > maior:
+            maior = lista[i]
+    
+    return maior
+
 def subsequencia_crescente_infectados(dados):
     n = len(dados)
     memo = [1] * n
@@ -58,11 +70,16 @@ def subsequencia_crescente_infectados(dados):
     for i in range(1, n):
         for j in range(i):
             if dados[i] > dados[j]:
-                memo[i] = max(memo[i], memo[j] + 1)
+                if memo[i] <= memo[j]+1:
+                    memo[i] = memo[j] + 1
+                 
     
     tam_mais_longa_subsequencia = maior_valor(memo)
-    indice = memo.index(tam_mais_longa_subsequencia)
-    
+    indice = 0
+    for x in range(n):
+        if memo[x] == tam_mais_longa_subsequencia:
+            indice = x
+
     subsequencia = [dados[indice]]
     for k in range(indice - 1, -1, -1):
         if memo[k] == tam_mais_longa_subsequencia - 1 and dados[k] < subsequencia[-1]:
